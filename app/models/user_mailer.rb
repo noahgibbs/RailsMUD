319c6d94c143fe017c111b9c8702184ce96593b8
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-05-22 01:38:49 -0700

    Improve signup stuff

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,22 +2,22 @@ class UserMailer < ActionMailer::Base
   def signup_notification(user)
     setup_email(user)
     @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
-  
+
+    @body[:url]  = ENV['RM_SITE_URL'] + "/activate/#{user.activation_code}"
+
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = ENV['RM_SITE_URL']
   end
-  
+
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = ENV['RM_ADMIN_EMAIL']
+      @subject     = ENV['RM_MAIL_PREFIX'] + " "
       @sent_on     = Time.now
       @body[:user] = user
     end
--- pv.sh
+++ pv.sh
@@ -1,11 +1,20 @@
+# The site address is where your server can be found.  It might be
+# something like freebeer.com or playbobomud.org.
+export RM_SITE_ADDRESS=railsmud.mydomain.com
+export RM_SITE_URL=http://$RM_SITE_ADDRESS
+
 # Set up information for your outbound mail server.  You can probably
 # find this information on your ISP's web site.  The username and password
 # is usually the one for your ISP.  The domain is the one you want your
-# outbound mail coming from -- something like freebeer.com or playbobomud.org.
+# outbound mail coming from.
 export RM_SMTP_SERVER=smtp.myisp.com
-export RM_SMTP_DOMAIN=myserverdomain.com
+export RM_SMTP_DOMAIN=$RM_SITE_ADDRESS
 export RM_SMTP_USER=joesmith
 export RM_SMTP_PASSWORD='j03_707411Y_r0xx0rz'
+export RM_MAIL_PREFIX='[MyMUD]'
 
 # The name for the original admin account
 export RM_ADMIN_NAME=joe_the_admin
+
+# The 'customer service' address your users are told to use
+export RM_ADMIN_EMAIL=joesmith@myisp.com
