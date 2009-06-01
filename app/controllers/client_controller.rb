2cfd2739c05a49b2bee35b677d1797ae605721d3
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-06-01 00:57:40 -0700

    Add Juggernaut stuff back in

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,11 +1,29 @@
 class ClientController < ApplicationController
   include AuthenticatedSystem
 
+  before_filter :requires_login, :only => 'full'
+
   def home
-    login_cookie  # Can't use as a before filter -- current_user wouldn't exist
+    # Can't use this func as a before filter -- current_user wouldn't exist
+    # because the restful_authentication before filters haven't been run yet.
+    login_cookie
+  end
+
+  def full
+  end
+
+  def send_chat_data
+    render :juggernaut do |page|
+      page.insert_html :top, 'chat_data', "<li>#{h params[:chat_input]}</li>"
+    end
+    render :nothing => true
   end
 
   private
+
+  def requires_login
+    logged_in?
+  end
 
   def login_cookie
     if current_user
