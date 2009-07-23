e2ace10784169b263c129eb61996c277b45f484c
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-07-23 14:09:25 -0700

    Add jugger_keys.rb for shared keys

---
+++ game/jugger_keys.rb
@@ -0,0 +1,8 @@
+module JuggernautConnect
+  GS_login_session_id = "this is a long string that's like a password " +
+    " between the game server and the juggernaut server, and the idea is " +
+    " that you shouldn't be able to just guess it, but it's only sent once " +
+    " so it can be really long to foil attackers, you know?  It's like, " +
+    " the longer it is, the stronger it is because cryptographic keys are " +
+    " like that.  Cool, huh?"
+end
