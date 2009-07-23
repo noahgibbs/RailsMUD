c5610c2a0721a2ed5b775e22ed705d3b2f6235f3
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-07-23 14:22:42 -0700

    Make key a legal chunk of URI

--- game/jugger_keys.rb
+++ game/jugger_keys.rb
@@ -1,8 +1,5 @@
 module JuggernautConnect
-  GS_login_session_id = "this is a long string that's like a password " +
-    " between the game server and the juggernaut server, and the idea is " +
-    " that you shouldn't be able to just guess it, but it's only sent once " +
-    " so it can be really long to foil attackers, you know?  It's like, " +
-    " the longer it is, the stronger it is because cryptographic keys are " +
-    " like that.  Cool, huh?"
+  # This string is a password between the Game server and the Rails
+  # server.
+  GS_login_session_id = "ab43253242342agdafhadsfasd423524325435dsfjasdfhjjjj"
 end
