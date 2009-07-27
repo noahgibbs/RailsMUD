0ab1760459d1de86b1fb0a2473bfe13a7c83a522
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-07-27 13:46:06 -0700

    Track set of players and rename get_names to get_names_from_objs

--- game/lib/player.rb
+++ game/lib/player.rb
@@ -5,6 +5,7 @@ require "juggernaut_connect.rb"
 
 class Player
   attr :login
+  @@players = []
 
   class << self
     include ActionView::Helpers::JavaScriptHelper # for javascript_escape
@@ -15,16 +16,28 @@ class Player
     @login = login
   end
 
-  def self.get_names(players)
-    players.map { |p|
-      p.kind_of?(String) ? p : p.login
-    }
+  def self.login(name, objects)
+    p = Player.new(name)
+    @@players[name] = p
+  end
+
+  def self.logout(name, objects)
+    @@players[name] = nil
   end
 
   def self.send_to_players(text, players)
     players = [players] unless players.kind_of? Array
 
     str = "try {\nadd_world_output(\"#{escape_javascript(text)}\");\n} catch (e) { alert('GS error:\\n\\n' + e.toString()); alert('add_world_output(\\\"\#{text}\\\");'); throw e }"
-    JuggernautConnect.send_to_clients(str, self.get_names(players))
+    JuggernautConnect.send_to_clients(str, self.get_names_from_objs(players))
   end
+
+  private
+
+  def self.get_names_from_objs(players)
+    players.map { |p|
+      p.kind_of?(String) ? p : p.login
+    }
+  end
+
 end
