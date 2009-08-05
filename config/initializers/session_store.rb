a6fcb021c15aa9d3d29c0440a978ef5c2eee8136
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-04 22:46:42 -0700

    Use secret env vars

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_railsmud_session',
-  :secret      => '9d848679fdc090d3207ba9ca3b3a5e5cfaef1cec401e4f666f6904fdd5550b43e5ba3a086a6dfcddd712b19582bbdbf5d4168fc4aa3097c7244297f3fbd1f732'
+  :secret      => ENV['RM_RAILS_SESSION_SECRET']
 }
 
 # Use the database for sessions instead of the cookie-based default,
--- game/jugger_keys.rb
+++ game/jugger_keys.rb
@@ -1,5 +1,5 @@
 module JuggernautConnect
   # This string is a password between the Game server and the Rails
   # server.
-  GS_login_session_id = "ab43253242342agdafhadsfasd423524325435dsfjasdfhjjjj"
+  GS_login_session_id = ENV['RM_JUGGER_GS_SESSION']
 end
