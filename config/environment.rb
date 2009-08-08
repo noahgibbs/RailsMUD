264bf4a18630578b3ad07f40bfcc7448da3be59a
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-07 21:57:26 -0700

    Change RailsGame to a gem plugin

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "rubyist-aasm", :lib => "aasm", :source => "http://gems.github.com"
   config.gem "daemons"
+  config.gem "railsgame"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
