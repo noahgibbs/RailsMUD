59fa531ff7b4364fb1e3dc5cf70b453a70f96601
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-17 14:41:20 -0700

    Update schema with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,39 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090522074933) do
+ActiveRecord::Schema.define(:version => 20090814214122) do
+
+  create_table "basic_exits", :force => true do |t|
+    t.integer  "basic_object_id",                :null => false
+    t.integer  "destination_id",                 :null => false
+    t.string   "type",            :limit => 80
+    t.string   "name",            :limit => 80
+    t.string   "description",     :limit => 240
+    t.text     "tags"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "basic_mobiles", :force => true do |t|
+    t.integer  "current_room_id"
+    t.integer  "current_body_id"
+    t.string   "name",            :limit => 80
+    t.string   "type",            :limit => 80
+    t.text     "tags"
+    t.text     "descriptions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "basic_objects", :force => true do |t|
+    t.string   "name",             :limit => 80
+    t.string   "type",             :limit => 80
+    t.string   "base_description", :limit => 240
+    t.text     "tags"
+    t.text     "descriptions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
