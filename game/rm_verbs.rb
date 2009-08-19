e465e35b188dee89ca547020ba1c2efb389e959c
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-18 20:55:05 -0700

    Add extra verbs, including one that lists database objects

--- game/rm_verbs.rb
+++ game/rm_verbs.rb
@@ -8,6 +8,41 @@ class RailsMUDServer < RailsGame::Server
     player.send_html("You think '#{h objects}'. <br />")
   end
 
+  def action_say(player, objects)
+    if player.location
+      player.location.show_text("#{player.name} says, '#{h objects}'. <br />")
+    else
+      player.send_html "You aren't anywhere, so your words are lost. <br/>"
+    end
+  end
+
+  def action_look(player, objects)
+    if objects.empty?
+      player.show_current_location
+      return
+    end
+
+    player.send_html("Not implemented yet. <br />")
+  end
+
+  def action_list_objects(player, objects)
+    unless objects.empty?
+      player.send_html("list_objects takes no parameters. <br />")
+      return
+    end
+
+    objs = BasicObject.find :all
+    if objs.empty?
+      player.send_html("There are no database-backed objects currently. <br/>")
+      return
+    end
+
+    objs_list = objs.map {|r| r.name}.join("</li> <li>")
+    objs_list = "<ul> <li>#{objs_list}</li> </ul>"
+
+    player.send_html "All objects:<br/>#{objs_list}<br/>"
+  end
+
   def unknown_action(player, verb, objects)
     player.send_html("I don't know the verb '#{h verb}'.  Sorry! <br />")
   end
