c11b398ce0043fbd672b473f182bfafc8af7c72a
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-09-01 21:27:17 -0700

    Minor updates, minor RailsGame mods

--- game/player.rb
+++ game/player.rb
@@ -4,7 +4,7 @@ require "railsgame"
 gem "activerecord"
 require "activerecord"
 
-require "start_location.rb"
+require "start_location"
 
 class BasicPlayer < BasicMobile
   include RailsGame::Player  # This sets BasicPlayer as the active Player class
